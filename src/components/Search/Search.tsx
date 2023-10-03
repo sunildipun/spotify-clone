5506c6b3d8dca3746014a58ec633d7b7a0870fa1
Sunil Behera <sunil.6318ie@gmail.com>
2023-10-03 23:29:39 +0530

    updated

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -10,6 +10,6 @@ const SearchInput = () => {
         <input className=" placeholder:text-slate-400 block bg-neutral-800 w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
         </label>
     )
-}
+};
 
 export default SearchInput;
